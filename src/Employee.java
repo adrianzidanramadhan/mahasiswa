c70c42961898c91b3cb12f7b8801dd64346909b3
Adrian Zidan <adrianzidan35@gmail.com>
2025-11-08 18:36:11 +0700

    add Manager

--- src/Employee.java
+++ src/Employee.java
@@ -70,20 +70,20 @@ public class Employee {
     }
 }
 
-class Main{
-    public static void main(String[] args) {
-        Employee emp1 = new Employee();
+// class Main{
+//     public static void main(String[] args) {
+//         Employee emp1 = new Employee();
 
-        emp1.setName("");
-        emp1.setAge(30);
-        emp1.setSalary(-2000000);
-        emp1.setActive(false);
-        emp1.setSkills(new String[]{"Java", "PHP", "JS"});
+//         emp1.setName("");
+//         emp1.setAge(30);
+//         emp1.setSalary(-2000000);
+//         emp1.setActive(false);
+//         emp1.setSkills(new String[]{"Java", "PHP", "JS"});
 
-        emp1.tampilkanInfo();
+//         emp1.tampilkanInfo();
 
-        emp1.setAge(20);
+//         emp1.setAge(20);
 
-        System.out.println("Umur: " + emp1.getAge());
-    }
-}
+//         System.out.println("Umur: " + emp1.getAge());
+//     }
+// }
---
+++ src/Manager.java
@@ -0,0 +1,23 @@
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, double salary, double bonus) {
+        super();
+        setName(name);
+        setSalary(salary);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Bonus: " + bonus);
+    }
+}
